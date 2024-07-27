1e7eca99762bc6586405ab18bf99392b9209b28c
B@C <aungthuyeinhein98@gmail.com>
2024-07-28 05:34:13 +0630

    feat: general error handler created

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+// User Define Module
+import CustomError from "./utils/customError.js";
+import globalErrorHandler from "./controllers/error.controller.js";
+
 const app = express();
 
 app.use(
@@ -14,10 +18,24 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("Test route is working!");
+});
+//404-Error Handler
+app.all("*", (req, res, next) => {
+  const err = new CustomError(
+    404,
+    `Can't find ${req.originalUrl} on the server!`
+  );
+  next(err);
+});
+
+app.use(globalErrorHandler);
 
 export default app;
