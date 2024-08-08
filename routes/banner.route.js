258648517330a5d957ca82d52964222dbe44f7bb
B@C <aungthuyeinhein98@gmail.com>
2024-08-08 08:53:31 +0630

    feat: protect banner upload api

--- routes/banner.route.js
+++ routes/banner.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { bannerUpload } from "../controllers/banner.controller.js";
 import { single } from "../middlewares/imageUploadMiddleware.js";
+import { protect } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/banners", single, bannerUpload);
+router.post("/banners", protect, single, bannerUpload);
 export default router;
