cb1cc9cc3751f048c1107162b7f54f4ec604a101
B@C <aungthuyeinhein98@gmail.com>
2024-08-15 15:31:07 +0630

    fix: change image upload middleware at routes

--- routes/companyLogo.route.js
+++ routes/companyLogo.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import domainExtracter from "../middlewares/domainExtracter.js";
-import { single } from "../middlewares/imageUploadMiddleware.js";
+import { singleImage } from "../middlewares/imageUploadMiddleware.js";
 import { protect } from "../controllers/auth.controller.js";
 import {
   companyLogoUpload,
@@ -12,10 +12,10 @@ import {
 
 const router = express.Router();
 
-router.post("/companies/logo", protect, single, companyLogoUpload);
+router.post("/companies/logo", protect, singleImage, companyLogoUpload);
 router.get("/companies/logo/public", domainExtracter, publicCompanyLogo);
 router.get("/companies/logo/cms", protect, cmsCompanyLogo);
 router.delete("/companies/logo/:id", protect, CompanyLogoDelete);
-router.patch("/companies/logo/:id", protect, single, CompanyLogoUpdate);
+router.patch("/companies/logo/:id", protect, singleImage, CompanyLogoUpdate);
 
 export default router;
