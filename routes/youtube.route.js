2ace21b48c818fdf0b14f11b1f93f36aa5e92163
B@C <aungthuyeinhein98@gmail.com>
2024-12-03 23:52:41 +0630

    feat: new api get by id ,update and delete

--- routes/youtube.route.js
+++ routes/youtube.route.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { addVideo, getAllVideos } from "../controllers/youtube.controller.js";
+import {
+  addVideo,
+  getAllVideos,
+  getVideoById,
+  deleteVideoById,
+  updateVideoById,
+} from "../controllers/youtube.controller.js";
 import { protect } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/", addVideo);
 router.get("/", getAllVideos);
+router.get("/:id", getVideoById);
+router.delete("/:id", deleteVideoById);
+router.patch("/:id", updateVideoById);
 
 export default router;
