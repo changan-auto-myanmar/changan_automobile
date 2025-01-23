4f59d072421413805c0fb6ef3999bdd6aea7c3f5
B@C <aungthuyeinhein98@gmail.com>
2025-01-23 15:31:53 +0630

    feat: change multitenent to single tentnet

--- models/csr.model.js
+++ models/csr.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const csrSchema = new Schema({
-  domainName: { type: String, required: true, trim: true, index: true },
+  // domainName: { type: String, required: true, trim: true, index: true },
 
   category: {
     type: String,
